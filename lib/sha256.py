90d11f6b4e93b1e2572557aa00d47da12eb953f3
polarbear08 <pistolstar0808@gmail.com>
2019-05-05 23:18:54 +0900

    [UPD] added hash initialization function

---
+++ lib/sha256.py
@@ -0,0 +1,30 @@
+"""
+Hash function SHA-256
+author: polarbear08
+
+"""
+
+# calculate initial hash value
+
+
+def calculate_initial_hash():
+    """Calculate initial 32byte hash."""
+    first_eight_primes = []
+    initial_hash = []
+
+    prime_generator = 1
+    block_num = 8
+    for _ in range(block_num):
+        first_eight_primes.append(next(prime_generator))
+
+
+
+
+
+
+
+# calculate round constants
+
+
+
+#
