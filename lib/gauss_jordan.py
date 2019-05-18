062c6b48c8f11ac4fe3d00ce634acc527e1b9315
polarbear08 <pistolstar0808@gmail.com>
2019-05-18 19:48:48 +0900

    [UPD] created logic of gauss_jordan

--- lib/gauss_jordan.py
+++ lib/gauss_jordan.py
@@ -14,5 +14,14 @@ def gauss_jordan(matrix):
         matrix: Gauss Jordan standard form of input matrix
 
     """
+    row, col = matrix.shape
+    for ri in range(row):
+        if not contain_nonzero(ri):
+            break
+
+        matrix = normalize_matrix(matrix)
+        pivot_row_idx = choose_pivot(matrix, ri)
+        matrix = exchange_rows(matrix, ri, pivot_row_idx)
+        matrix = make_othercol_zero(matrix, ri)
 
     return matrix
