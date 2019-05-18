6a594fcf39be4aa44936cb5e7349a78644c187b9
polarbear08 <pistolstar0808@gmail.com>
2019-05-18 19:41:43 +0900

    [UPD] created function gauss_jordan

--- lib/gauss_jordan.py
+++ lib/gauss_jordan.py
@@ -1,2 +1,18 @@
 # -*- coding: utf8 -*-
 
+__author__ = "polarbear08"
+__date__ = "19 May 2019"
+
+
+def gauss_jordan(matrix):
+    """Return Gauss Jordan standard form of the input matrix
+
+    Args:
+        matrix: 2 dimension matrix
+
+    Returns:
+        matrix: Gauss Jordan standard form of input matrix
+
+    """
+
+    return matrix
